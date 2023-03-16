cc64070cd800388dc5b8e9d7038218ef91e76113
mahmoud <mahmoud.m3798@gmail.com>
2023-03-16 16:49:45 -0700

    C First day

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry Point 
+ *
+ * Description: Program to print using printf function.
+ *
+ * Return: default 0 means (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
