78572c935cc479e685a88b6971d4b2f65bc7dc09
mahmoud <mahmoud.m3798@gmail.com>
2023-03-16 16:31:46 -0700

    C First day

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Program to print line using put function
+ *
+ * Return: 0 means success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
