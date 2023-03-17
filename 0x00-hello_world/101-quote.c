ec449572ada8d28e889f8f5b519d481eaf02ce01
mahmoud <mahmoud.m3798@gmail.com>
2023-03-16 18:11:17 -0700

    C First day

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: C program to print string without printf or puts
+ *
+ * Return: 1
+*/
+
+int main(void)
+{
+	char s[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,s.59);
+	return (1);
+}
