d38c62f240501e8213af56f400fea3d62df02381
mahmoud <mahmoud.m3798@gmail.com>
2023-03-17 17:21:05 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Entry point 
+*
+* Description: prints all possible combinations of two two-digit numbers.
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			putchar(n + '0');
+			putchar(n + '0');
+			putchar(' ');
+			putchar(m + '0')
+			putchar((m + 1) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
