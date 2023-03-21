a0b8455872a2be24743b14fb0d6d4bbc09278f26
mahmoud <mahmoud.m3798@gmail.com>
2023-03-21 16:20:43 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main -Entry Point
+*
+* Description: prints _putchar, followed by a new line.
+*
+* Return: always 0
+*/
+int main(void)
+{
+	int c;
+	char word[] = "_putchar";
+
+	for (c = 0; c < 8; c++)
+		_putchar(word[c]);
+	_putchar('\n');
+	return (0);
+}
