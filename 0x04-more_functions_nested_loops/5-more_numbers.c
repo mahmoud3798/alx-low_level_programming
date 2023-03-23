7dbeefe934a65c9d911cc63a157ea988f897fee8
mahmoud <mahmoud.m3798@gmail.com>
2023-03-23 09:57:56 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,20 @@ void more_numbers(void)
 {
 	int c;
 	int r;
+	int x;
 
 	for (r = 0; r < 10; r++)
 	{
 		for (c = 0; c <= 14; c++)
-			_putchar(c + 48);
+		{
+			x = c;
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				x = c % 10;
+			}
+			_putchar(x + 48);
+		}
 		_putchar('\n');
 	}
 }
