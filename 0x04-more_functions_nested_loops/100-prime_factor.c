0bdad9a2ae624172353b2de0cb3764e16ec208e7
mahmoud <mahmoud.m3798@gmail.com>
2023-03-23 16:11:20 -0700

    11. The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -31,15 +31,14 @@ void maxPrimeFactors(long int n)
 
 	while (n % 2 == 0)
 	{
-		maxPrime = 2;
 		n = n / 2;
 	}
 	for (i = 3; i <= squareRoot(n); i += 2)
 	{
 		while (n % i == 0)
 		{
-			maxPrime = i;
 			n = n / i;
+			maxPrime = i;
 		}
 	}
 	if (n > 2)
