e09301631ef2042cc32036d1c38b32f9b4aafefb
mahmoud <mahmoud.m3798@gmail.com>
2023-03-23 13:04:33 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,17 @@ int main(void)
 	for (r = 1; r <= 100; r++)
 	{
 		if (r % 3 == 0 && r % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (r % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (r % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", r);
+			printf("%d", r);
+		if (r != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
