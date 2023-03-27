64c4709bce05f05cc13c63c2fe770ac9998fc462
mahmoud <mahmoud.m3798@gmail.com>
2023-03-27 13:53:19 -0700

    1. Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
 * swap_int - function that swaps the values of two integers.
-* @a - first variable
-* @b - second variable 
+* @a: first variable
+* @b: second variable
 */
 void swap_int(int *a, int *b)
 {
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _strlen - function that returns the length of a string.
+* @s - string variable
+*/
+
+int _strlen(char *s)
+{
+	
+}
