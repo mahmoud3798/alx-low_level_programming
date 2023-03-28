6c3d9971854ffbdca93a754a04e304e7848a6cde
mahmoud <mahmoud.m3798@gmail.com>
2023-03-27 19:10:35 -0700

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 			sy *= -1;
 		else if (*s >= '0' && *s <= '9')
 			n = (n * 10) + (*s - '0');
-		else if (num > 0)
+		else if (n > 0)
 			break;
 	} while (*s++);
 	return (n * sy);
