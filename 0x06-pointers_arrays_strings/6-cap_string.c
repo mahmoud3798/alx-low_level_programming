e701b974e48d8405a8fb162f0d9353186dbd24a0
mahmoud <mahmoud.m3798@gmail.com>
2023-03-30 16:49:43 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+* Deli - to check whether the char is delimeter or not
+* @s: char variable
+* Return: 1 if char is delimeter, otherwise 0
+*/
+int Deli(char s)
+{
+	char d[] = " \t\n;,.!?\"(){}";
+	int i;
+
+	for (i = 0; i < 13; i++)
+		if (s == d[i])
+			return (1);
+	return (0);
+}
+/**
+* low - to check whether the char is lowercase
+* @l: char variable
+* Return: 1 if char is delimeter, otherwise 0
+*/
+int low(char l)
+{
+	if (l >= 97 && l <= 122)
+		return (1);
+	else
+		return (0);
+}
+/**
+* cap_string - function that capitalizes all words of a string.
+* @s: char pointer
+* Return: char pointer
+*/
+char *cap_string(char *s)
+{
+	char *p = s;
+	int ISdelimeter = 1;
+
+	while (*s)
+	{
+		if (ISdelimeter && low(*s))
+		{
+			*s -= 32;
+			ISdelimeter = 0;
+		}
+		else if (Deli(*s))
+			ISdelimeter = 1;
+		else
+			ISdelimeter = 0;
+		s++;
+	}
+	return (s);
+}
