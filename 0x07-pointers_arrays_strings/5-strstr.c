1ee49dde11dc12af947e49a99750a8fd85c76fa8
mahmoud <mahmoud.m3798@gmail.com>
2023-04-03 18:55:50 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+* _strcmp - function that compares two strings.
+* @s1: char pointer
+* @s2: char pointer
+* @c: integer variable
+* Return: integer
+*/
+int _strcmp(char *s1, char *s2, unsigned int c)
+{
+	int cmp = 0;
+
+	while (*s1 && cmp <= c)
+	{
+		if (*s1 != *s2)
+		{
+			cmp = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (cmp - c);
+}
+/**
 * _strstr - function that locates a substring.
 * @haystack: char pointer
 * @needle: char pointer
@@ -8,5 +31,15 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
+	unsigned int i = 0;
 
+	while (needle[i] != '\0')
+		i++;
+	while (*haystack != '\0')
+	{
+		if (_strncmp(haystack, needle, i) == 0)
+			return (haystack);
+		haystack++;
+	}
+	return ('\0');
 }
