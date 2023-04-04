7cf8bcbf2f502f0aee5bda29623b11a6453c280e
mahmoud <mahmoud.m3798@gmail.com>
2023-04-03 17:57:33 -0700

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _strstr - function that locates a substring.
+* @haystack: char pointer
+* @needle: char pointer
+* Return: char pointer
+*/
+char *_strstr(char *haystack, char *needle)
+{
+
+}
