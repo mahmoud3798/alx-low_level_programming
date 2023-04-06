1cf8c69989540b631c8f5e41c502792297d1041d
mahmoud <mahmoud.m3798@gmail.com>
2023-04-05 17:15:08 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -18,10 +18,10 @@ int is_prime_number(int n)
 */
 int prime(int x, int y)
 {
-	if (x % y == 0 || x <= 1)
-		return (0);
-	else if (y >= x && x > 1)
+	if (y >= x && x > 1)
 		return (1);
+	else if (x % y == 0 || x <= 1)
+		return (0);
 	else
 		return (prime(x, y + 1));
 }
