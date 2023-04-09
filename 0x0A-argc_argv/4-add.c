17a5267e6b52a96cda341637cd6b0dc530c8910c
mahmoud <mahmoud.m3798@gmail.com>
2023-04-09 05:49:27 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* @argc: integer variable
+* @argv: array of chars
+* Return: always 0
+*/
+int main(int argc, char const *argv[])
+{
+	int res = 0;
+	char *ch;
+
+	while (--argc)
+	{
+		ch = argv[argc];
+		while (*ch)
+		{
+			if (*c < '0' || *c > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			ch++;
+		}
+		res += atoi(argv[argc]);
+	}
+	printf("%d\n", res);
+	return (0);
+}
