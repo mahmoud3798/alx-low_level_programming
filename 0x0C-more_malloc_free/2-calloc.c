81c3ea22bfbee1774cb4852a4992edc78b5a02c5
mahmoud <mahmoud.m3798@gmail.com>
2023-04-13 10:54:29 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,9 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *ptr = s;
-	unsigned int i;
 
-	for (i = 0; n > 0; n--, i++)
-	{
-		s[i] = b;
-	}
+	while (n--)
+		*s++ = b;
 	return (ptr);
 }
 /**
@@ -28,14 +25,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *ptr;
 
 	if (size == 0 || nmemb == 0)
-	{
 		return (NULL);
-	}
 	ptr = malloc(sizeof(int) * nmemb);
 	if (ptr == 0)
-	{
 		return (NULL);
-	}
-	_memset(ptr, 0, (sizeof(int) * nmemb));
+	_memset(ptr, 0, sizeof(int) * nmemb);
 	return (ptr);
 }
