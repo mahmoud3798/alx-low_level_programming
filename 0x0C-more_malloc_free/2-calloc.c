db25e4d369c111879db3c404380ba480c25dcb59
mahmoud <mahmoud.m3798@gmail.com>
2023-04-13 19:57:33 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(sizeof(int) * nmemb + 1);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb ; i++)
+	for (i = 0; i <= nmemb ; i++)
 		ptr[i] = 0;
 	return (ptr);
 }
