06cc32b96743332eceb782037f0b58a020e64506
mahmoud <mahmoud.m3798@gmail.com>
2023-04-13 19:56:45 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(sizeof(int) * nmemb);
+	ptr = malloc(sizeof(int) * nmemb + 1);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb ; i++)
