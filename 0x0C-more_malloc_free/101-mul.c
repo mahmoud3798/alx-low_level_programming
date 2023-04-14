e15969bbed79fed7aa4940ef7f61b8adc965fc10
mahmoud <mahmoud.m3798@gmail.com>
2023-04-13 20:14:48 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* _digit - check string
+* @argv: pointer
+* Return: return 0 ,or 1
+*/
+int _digit(char *argv)
+{
+	int i = 0;
+
+	while (argv[i])
+	{
+		if (argv[i] >= '0' && argv[i] <= '9')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+/**
+* _atoi - function converts a string
+* @s: pointer
+* Return: integer
+*/
+int _atoi(char *s)
+{
+	int i, res = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res *= 10;
+			res += (s[i] - '0');
+		}
+	}
+	return (res);
+}
+/**
+* main - Entry point
+* @argc: int parameter
+* @argv: array of char
+* Return: return 0 on success, or 98
+*/
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	malloc();
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	while (i < argc)
+	{
+		if (_digit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		i++;
+	}
+	return (0);
+}
