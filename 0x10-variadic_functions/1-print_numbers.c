bfcb5d40c8a0d31a551810c6ea56a502d251462b
mahmoud <mahmoud.m3798@gmail.com>
2023-04-20 12:03:54 -0700

    0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,8 +1,24 @@
 #include "variadic_functions.h"
 /**
-* sum_them_all - function that returns the sum of all its parameters.
+* print_numbers - function that prints numbers, followed by a new line.
 * @n: # of args
 * @...: integers
-* Return: sum
+* @separator: char pointer
 */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int m = n;
+	va_list p;
 
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(p, n);
+	while (m--)
+	{
+		printf("%d%s", va_arg(p, int), m ? (separator ? separator : "") : "\n");
+	}
+	va_end(p)
+}
