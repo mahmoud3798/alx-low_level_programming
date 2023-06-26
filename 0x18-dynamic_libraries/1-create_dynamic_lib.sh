1deac308619d35f70632d39c3dbf85a2851204c1
mahmoud <mahmoud.m3798@gmail.com>
2023-06-26 06:37:52 -0700

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-gcc -shared -o liball.so *.c
+
+# Compile all .c files in the current directory into object files
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create the dynamic library from the object files
+gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
