b14097e9bc24e4377e65b7b4b456193be20f9b68
mahmoud <mahmoud.m3798@gmail.com>
2023-07-07 14:01:49 -0700

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+* hash_table_print: function that prints a hash table.
+* @ht: hash table
+*
+* Return: void
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp = NULL;
+	unsigned int ind = 0;
+	char *s = "";
+
+	if (!ht)
+		return;
+	putchar('{');
+
+	while (ind < ht->size)
+	{
+		tmp = ht->array[ind];
+		if (tmp)
+			printf("%s", s);
+		while (tmp)
+		{
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			if (tmp->next)
+				printf("%s", s);
+			tmp = tmp->next;
+			s = ", ";
+		}
+		ind++;
+	}
+	putchar('}');
+	putchar('\n');
+}
